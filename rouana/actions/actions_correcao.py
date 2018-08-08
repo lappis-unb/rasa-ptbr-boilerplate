6143076799ac4952952dd3a1612ddddad002c6bb
palomacmamede <42178586+palomacmamede@users.noreply.github.com>
2018-08-08 12:05:40 -0300

    revisão do texto + alterações de frases

--- rouana/actions/actions_correcao.py
+++ rouana/actions/actions_correcao.py
@@ -2,11 +2,11 @@ from actions.actions_geral import ActionMultiline
 
 class ActionExplicarCorrecao(ActionMultiline):
     messages = [
-        'Estamos na etapa de correção, aqui posso responder dúvidas '
-        'de campos preenchidos incorretamente ou motivos para sua proposta '
-        'não ser aceita. Vou tentar te ajudar a corrigí-los.',
+        'Estamos na etapa de correção, aqui posso responder suas dúvidas sobre '
+        'campos preenchidos incorretamente ou motivos para sua proposta '
+        'não ser aceita. Que tal corrigirmos isso juntos? :smile: ',
 
-        'Mas, por enquanto, eu não consigo te responder a respeito deste '
+        'Mas, por enquanto, eu ainda não consigo te responder a respeito deste '
         'assunto.'
     ]
 
