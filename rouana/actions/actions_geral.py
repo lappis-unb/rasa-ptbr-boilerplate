2294a59af6b3f7378f9e426febe11483bbe0a6d3
ArthurTemporim <arthurrtl@gmail.com>
2018-08-15 11:19:28 -0300

    Add break lines in domain and actions_geral.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- rouana/actions/actions_geral.py
+++ rouana/actions/actions_geral.py
@@ -30,14 +30,13 @@ class ActionDefinirPerfil(ActionMultiline):
     messages = [
 
         'Você prefere conversar sobre criação e andamento de projetos, ou '
-
         'prefere falar mais sobre a Lei Rouanet?'
     ]
 
 class ActionExplicarDefinicaoPerfil(ActionMultiline):
     messages = [
         'Se você quiser saber mais sobre a lei Rounet, nós podemos descobrir juntos sobre como ela foi criada,'
-        'o que ela proporciona, de onde vem o dinheiro usado na lei, e coisas afins.',
+        'o que ela proporciona, de onde vem o dinheiro usado na lei, e coisas afins.\n'
 
         'Se você preferir falar sobre criação e andamento de projetos, podemos analisar onde a sua dúvida se encaixa.'
         'Por exemplo, talvez você queira saber mais sobre como funciona um andamento de projeto, ou como submeter um projeto, entre outras.',
@@ -69,7 +68,7 @@ class ActionCadastroSalic(ActionMultiline):
 
 class ActionDespedir(ActionMultiline):
     messages = [
-        'Espero que eu tenha conseguido esclarecer suas dúvidas. '
+        'Espero que eu tenha conseguido esclarecer suas dúvidas. \n'
         'Ainda estou em fase de teste, a cada dia aprendo mais com as conversas que tenho com vocês, '
         'se surgir alguma dúvida, não hesite, volte aqui',
 
@@ -79,27 +78,27 @@ class ActionDespedir(ActionMultiline):
 class ActionSubmissaoDeProjetos(ActionMultiline):
     messages = [
         'Tudo bem. Aqui vai um resumo do processo:',
-        'A pessoa responsável, chamada de proponente, insere uma proposta cultural no Sistema de Apoio às Leis de Incentivo à Cultura (Salic), de forma eletrônica.',
-        'Essa proposta é analisada pelo MinC. Se for aprovada, autoriza o proponente a captar recursos.',
-        'Assim que o valor mínimo for captado, o proponente pode começar a executar o projeto.',
-        'Durante a execução o proponente deve prestar contas de acordo com os prazos definidos na Lei Rouanet.',
-        'A prestação de contas é analisada pelos técnicos do Ministério e se aprovada, permite que o proponente crie um novo projeto.',
+        'A pessoa responsável, chamada de proponente, insere uma proposta cultural no Sistema de Apoio às Leis de Incentivo à Cultura (Salic), de forma eletrônica.\n'
+        'Essa proposta é analisada pelo MinC. Se for aprovada, autoriza o proponente a captar recursos.\n'
+        'Assim que o valor mínimo for captado, o proponente pode começar a executar o projeto.\n'
+        'Durante a execução o proponente deve prestar contas de acordo com os prazos definidos na Lei Rouanet.\n'
+        'A prestação de contas é analisada pelos técnicos do Ministério e se aprovada, permite que o proponente crie um novo projeto.\n'
         'O primeiro passo para você particiar deste processo é ter cadastro no SALIC'
     ]
 
 class ActionDefinirContexto(ActionMultiline):
     messages = [
-        'Onde sua pergunta se encaixa melhor:',
-        '1. Processo e estado do projeto',
-        '2. Preenchimento de proposta',
-        '3. Datas e Prazos',
-        '4. Erros do SALIC',
-        '5. Não sei bem onde se encaixa',
+        'Onde sua pergunta se encaixa melhor:\n'
+        '1. Processo e estado do projeto\n'
+        '2. Preenchimento de proposta\n'
+        '3. Datas e Prazos\n'
+        '4. Erros do SALIC\n'
+        '5. Não sei bem onde se encaixa\n'
     ]
 
 class ActionAviso(ActionMultiline):
     messages = [
-        'Para eu ser mais eficiente na solução da sua dúvida vou fazer algumas perguntas.',
+        'Para eu ser mais eficiente na solução da sua dúvida vou fazer algumas perguntas.\n'
 
         'Você já preencheu uma proposta? (Sim ou Não)',
     ]
@@ -109,11 +108,11 @@ class ActionIntroduzirExecucao(ActionMultiline):
         'Eu ainda não aprendi a falar sobre um projeto que está em Execução ou'
         'em Prestação de Contas. Ainda estou aprendendo... '
         'Em breve eu poderei responder sua dúvida. '
-        'Neste momento, eu recomendo que vocẽ entre em contato com as equipes da SEFIC:',
+        'Neste momento, eu recomendo que vocẽ entre em contato com as equipes da SEFIC:\n'
 
-        'Execução: acompanhamento.incentivo@cultura.gov.br',
+        'Execução: acompanhamento.incentivo@cultura.gov.br\n'
 
-        'Prestação de Contas: prestacaodecontas.incentivo@cultura.gov.br',
+        'Prestação de Contas: prestacaodecontas.incentivo@cultura.gov.br\n'
     ]
 
 class ActionJaEhProponente(ActionMultiline):
@@ -123,14 +122,14 @@ class ActionJaEhProponente(ActionMultiline):
 
 class ActionCadastroProponenteIntroduzirContexto(ActionMultiline):
     messages = [
-        'Maravilha!',
+        'Maravilha!\n'
         'Agora eu posso tentar esclarer algumas de suas dúvidas',
     ]
 
 class ActionCadastroSalicVideo(ActionMultiline):
     messages = [
         'O Salic é o Sistema de Apoio às Leis de Incentivo à Cultura, e é por '
-        'ele que você cadastra a proposta e a acompanha',
+        'ele que você cadastra a proposta e a acompanha\n'
 
         'Se você encontrar dificuldades no momento do cadastro, pode assistir '
         'este vídeo, nele você pode observar o passo a passo para se cadastrar:',
@@ -142,16 +141,16 @@ class ActionCadastroSalicVideo(ActionMultiline):
 
 class ActionExplicarCadastroSalic(ActionMultiline):
     messages = [
-        'A primeira coisa que você deve fazer é acessar o SALIC no site http://salic.cultura.gov.br',
+        'A primeira coisa que você deve fazer é acessar o SALIC no site http://salic.cultura.gov.br\n'
 
         'Então no formulário inicial, onde pede login e senha, você deve'
-        'clicar na opção "Não sou cadastrado", logo abaixo',
+        'clicar na opção "Não sou cadastrado", logo abaixo\n'
 
         'Preencha todos os campos corretamente, todos eles são obrigatórios.'
-        'E depois clique em "Cadastrar"',
+        'E depois clique em "Cadastrar"\n'
 
         'Agora aguarde a chegada de um e-mail de confirmação. Este vai conter '
-        'uma senha temporária',
+        'uma senha temporária\n'
 
         'É interessante você alterar esta senha, por isso, no seu primeiro '
         'acesso, vá em "Usuário > Alterar Senha", para mudar a mesma',
@@ -161,15 +160,15 @@ class ActionExplicarCadastroSalic(ActionMultiline):
 
 class ActionCadastroSalicAposVideo(ActionMultiline):
     messages = [
-        'Espero que o vídeo tenha ajudado',
+        'Espero que o vídeo tenha ajudado\n'
         'Agora que você já se cadastrou, o próximo passo cadastrar um proponente'
     ]
 
 class ActionInscricaoProponente(ActionMultiline):
     messages = [
-        'Para inscrever um proponente, ou se tornar um proponente, você deve:',
+        'Para inscrever um proponente, ou se tornar um proponente, você deve:\n'
 
-        'Acessar o SALIC e efetuar o login',
+        'Acessar o SALIC e efetuar o login\n'
 
         'Clique em Administrativo e depois em cadastrar proponente. OU acesse esse link http://salic.cultura.gov.br/agente/agentes/incluiragente;',
 
@@ -179,14 +178,14 @@ class ActionInscricaoProponente(ActionMultiline):
 
 class ActionIntroduzirContextoNovaProposta(ActionMultiline):
     messages = [
-        'Que bom! É sempre bom trabalhar em novos projetos culturais',
+        'Que bom! É sempre bom trabalhar em novos projetos culturais\n'
 
         'Se você quiser, posso tirar algumas de suas dúvidas para te ajudar neste novo projeto'
     ]
 
 class ActionIntroduzirContextoNaoExecucao(ActionMultiline):
     messages = [
-        'Então suas dúvidas devem ser sobre as outras etapas do processo',
+        'Então suas dúvidas devem ser sobre as outras etapas do processo\n'
 
         'Vamos saná-las juntos? (Sim ou Não)'
     ]
@@ -209,14 +208,14 @@ class ActionEscolheuContextoErrado(ActionMultiline):
 class ActionExplicarContextos(ActionMultiline):
     messages = [
         'Na categoria "Processo" eu responderei a respeito '
-        'do fluxo de submissão do seu projeto, e o sobre o estado dele.',
+        'do fluxo de submissão do seu projeto, e o sobre o estado dele.\n'
 
         'Em "Preenchimento" eu vou tirar dúvidas sobre o '
         'preenchimento de propostas no Salic, campos em que você possa ter '
-        'dúvidas, ou como você pode preencher',
+        'dúvidas, ou como você pode preencher\n'
 
         'Agora, em Datas e Prazos, eu consigo falar sobre prazos importantes '
-        'para a aceitação e execução do seu projeto.',
+        'para a aceitação e execução do seu projeto.\n'
 
         'Por último, em erros do SALIC, posso falar sober eventuais erros que '
         'ocorrem durante a utilização do sistema.',
@@ -231,9 +230,9 @@ class ActionExtra(ActionMultiline):
 
 class ActionAindaNaoAprendi(ActionMultiline):
     messages = [
-        'Eu ainda não aprendi a falar sobre este tópico...',
+        'Eu ainda não aprendi a falar sobre este tópico...\n'
 
-        'Em breve eu poderei responder sua dúvida',
+        'Em breve eu poderei responder sua dúvida\n'
 
         'Neste momento, eu recomendo que vocẽ entre em contato com as equipes da SEFIC: http://rouanet.cultura.gov.br/fale-conosco/',
     ]
