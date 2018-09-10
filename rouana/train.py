fa10b42dd5993ec1ccd478f58d2cfb325edf88a5
Diego Dorgam <diego.dorgam@rocket.chat>
2018-09-10 15:50:58 -0300

    aumentando epochs e resolvendo typos no domain.yml

--- rouana/train.py
+++ rouana/train.py
@@ -11,14 +11,14 @@ from rasa_core.policies.keras_policy import KerasPolicy
 from rasa_core.policies.memoization import MemoizationPolicy
 
 logger = logging.getLogger(__name__)
-TRAINING_EPOCHS = int(os.getenv('TRAINING_EPOCHS', 400))
+TRAINING_EPOCHS = int(os.getenv('TRAINING_EPOCHS', 750))
 
 def train_dialogue(domain_file='domain.yml',
                    model_path='models/dialogue',
                    training_data_file='data/stories'):
-    fallback = FallbackPolicy(fallback_action_name="action_default_fallback",
-                              core_threshold=0.12,
-                              nlu_threshold=0.12)
+    fallback = FallbackPolicy(fallback_action_name="utter_default",
+                              core_threshold=0.20,
+                              nlu_threshold=0.20)
 
     agent = Agent(
         domain_file,
