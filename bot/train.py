c21e74616248a4ffa5b79b31901cbe15d95a2ed1
Diego Dorgam <diego.dorgam@rocket.chat>
2018-11-12 15:29:31 -0200

    fix train.py

--- bot/train.py
+++ bot/train.py
@@ -39,7 +39,7 @@ def train_core(domain_file, model_path, training_folder):
     agent = Agent(domain_file, policies=[
             keras_2,
             MemoizationPolicy(max_history=MAX_HISTORY),
-                                                    CustomFallbackPolicy(
+            CustomFallbackPolicy(
                         fallback_action_name=FALLBACK_ACTION_NAME,
                         nlu_threshold=NLU_THRESHOLD,
                         core_threshold=CORE_THRESHOLD)])
@@ -53,4 +53,4 @@ def train_core(domain_file, model_path, training_folder):
 
 
 if __name__ == "__main__":
-    train_dialogue('domain.yml', 'models/dialogue', 'data/stories/')
+    train_core('domain.yml', 'models/dialogue', 'data/stories/')
