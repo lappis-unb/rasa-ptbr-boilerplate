c953ae5dea56f7f9891c4874f022ef529d732094
ArthurTemporim <arthurrtl@gmail.com>
2018-11-26 19:59:43 -0200

    Improve test.py and create util.py to modularize tests
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>
    Co-authored-by: Bruna Pinos <bruna.pinos@gmail.com>

--- bot/test/test.py
+++ bot/test/test.py
@@ -1,46 +1,10 @@
 import pytest
-import requests
-import simplejson as json
+import util
 
-def send_message(message):
-    request=""
-    try:
-        URL = 'http://localhost:5005/conversations/default/respond'
-        PARAMS = {'query':message}
-        request = requests.get(url = URL, params = PARAMS)
-    except ConnectionError:
-        print(ConnectionError)
-    return request.json()
-
-def get_data():
-    URL = 'http://localhost:5005/conversations/default/tracker'
-    event_tracker = []
-    try:
-        request = requests.get(url = URL)
-        data = request.json()
-        for e in data['events']:
-            if 'event' in e:
-                if 'action' in e['event']:
-                    event_tracker.append(['utter', e['name']])
-            if 'parse_data' in e:
-                event_tracker.append(['user_message', e['text']])
-                event_tracker.append(['intent', e['parse_data']['intent']['name']])
-                event_tracker.append(['confidence', e['parse_data']['intent']['confidence']])
-    except ValueError:
-        print("Error while genting data from RASA API!!")
-
-    return event_tracker
-
-def send_multiple_messages(messages):
-    for userMessage in messages:
-        send_message(userMessage)
+bot_data = util.start(['oi', 'o que é salic?'])
 
 def test_cumprimentar():
-    multiplePaths = ['oi', 'o que é salic?']
-    send_multiple_messages(multiplePaths)
-    print(get_data())
-    assert get_data()[2][1] == 'cumprimentar'
+    assert bot_data[1]['intent'] == 'cumprimentar'
 
 def test_salic():
-    assert get_data()[8][1] == 'definicao_salic'
-
+    assert bot_data[5]['intent'] == 'definicao_salic'
---
+++ bot/test/util.py
@@ -0,0 +1,56 @@
+import requests
+import simplejson as json
+
+"""
+The result of the function get_tracker_data() is a list of dictionaries,
+where each utter is in a single dict and each user data like user_message,
+intent and confidence is in the same dict.
+Here is an example of output:
+
+[{'utter': 'action_listen'}, 
+{'user_message': 'oi', 'intent': 'cumprimentar', 'confidence': 0.8761389255523682}, 
+{'utter': 'utter_cumprimentar'}, 
+{'utter': 'utter_menu'}, 
+{'utter': 'action_listen'}]
+"""
+
+def get_tracker_data():
+    URL = 'http://localhost:5005/conversations/default/tracker'
+    event_tracker = []
+    try:
+        request = requests.get(url = URL)
+        data = request.json()
+        for e in data['events']:
+            event={}
+            if 'event' in e:
+                if 'action' in e['event']:
+                    event['utter']=e['name']
+            if 'parse_data' in e:
+                event['user_message']=e['text']
+                event['intent']=e['parse_data']['intent']['name']
+                event['confidence']=e['parse_data']['intent']['confidence']
+            if event != {}:
+                event_tracker.append(event)
+    except ValueError:
+        print("Error while genting data from RASA API!!")
+
+    return event_tracker
+
+def send_message(message):
+    request=""
+    try:
+        URL = 'http://localhost:5005/conversations/default/respond'
+        PARAMS = {'query':message}
+        request = requests.get(url = URL, params = PARAMS)
+    except ConnectionError:
+        print(ConnectionError)
+    return request.json()
+
+def send_multiple_messages(messages):
+    for userMessage in messages:
+        send_message(userMessage)
+
+def start(messages):
+    send_multiple_messages(messages)
+    print(get_tracker_data())
+    return get_tracker_data()
