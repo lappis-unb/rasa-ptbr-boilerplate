05a6be50b910e9681f9623d370b4b6149e581abe
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-12-05 10:48:04 -0200

    Adds functions for utters
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- bot/validator.py
+++ bot/validator.py
@@ -35,10 +35,24 @@ parser.add_argument(
     help='Path for the intents file or directory'
 )
 
-
 parser.add_argument(
     '--warnings', '-w', type=str2bool, default=True,
     help='Warings flag'
+) 
+
+parser.add_argument(
+    '--validate_intents', type=str2bool, default=True,
+    help='Run validations to intents'
+) 
+
+parser.add_argument(
+    '--validate_utters', type=str2bool, default=True,
+    help='Run validations to utters'
+) 
+
+parser.add_argument(
+    '--validate_domain', type=str2bool, default=True,
+    help='Run validations to domain'
 ) 
 
 class Validator:
@@ -46,6 +60,7 @@ class Validator:
     intents = []
     stories = []
     valid_intents = []
+    valid_utters = []
 
     def __init__(self, domain_path, intents_path, stories_path):
         self.domain = domain_path
@@ -129,7 +144,7 @@ class Validator:
         for intent in intents_in_domain:
             found = self.search(intents_in_files, intent)
             if not found:
-                logger.error('Intent ' + intent + ' is in the domain file but was'+
+                logger.error('The intent ' + intent + ' is in the domain file but was'+
                           ' not found in the intent files')
             else:
                 self.valid_intents.append(intent)
@@ -137,10 +152,8 @@ class Validator:
         for intent in intents_in_files:
             found = self.search(intents_in_domain, intent)
             if not found:
-                logger.error('Intent ' + intent + ' is in the intent files but was'+
+                logger.error('The intent ' + intent + ' is in the intent files but was'+
                              ' not found in the domain file')
-            else:
-                self.valid_intents.append(intent)
 
     def verify_intents_in_stories(self):
         if self.valid_intents == []:
@@ -162,7 +175,7 @@ class Validator:
                     found = self.search(self.valid_intents, intent)
                     if not found:
                         logger.error('The intent '+ intent +' is used in the stories'+
-                                     ' file '+ file + ' (line: '+
+                                     ' story ile '+ file + ' (line: '+
                                      str(stories_lines.index(line)+1) +
                                      ') but it\'s not a valid intent.')
 
@@ -189,27 +202,121 @@ class Validator:
             if not found:
                 logger.warning('The intent ' + intent + ' is not being used in any story')
 
+    def verify_utters(self): 
+        file = open(self.domain, 'r')
+        domain_lines = file.readlines()
+        file.close()
+        utter_actions = []
+        utter_templates = []
+        
+        start = domain_lines.index('templates:\n') + 1
+        for i in range(start, len(domain_lines)):
+            line = domain_lines[i]
+            s_line = line.strip()
+            if s_line.split('_')[0] == 'utter':
+                utter = s_line
+                if utter.endswith(':'):
+                    utter = utter[:utter.find(':')]
+                utter_templates.append(utter)
+            elif (len(s_line.split()) >= 1 and  s_line.split()[0] != '-') and (s_line.endswith(':') and not line.startswith(' ')):
+                break
+
+        start = domain_lines.index('actions:\n') + 1
+        for i in range(start, len(domain_lines)):
+            line = domain_lines[i]
+            s_line = line.split()
+            if len(s_line) == 2 and s_line[0] == '-':
+                ss_line = s_line[1].split('_')
+                if ss_line[0] == 'utter':
+                    utter_actions.append(s_line[1])
+            elif line.strip().endswith(':'):
+                break
+
+        for utter in utter_actions:
+            found = self.search(utter_templates, utter)
+            if not found:
+                logger.error('There is no template for utter ' + utter)
+            else:
+                self.valid_utters.append(utter)
+        
+        for utter in utter_templates:
+            found = self.search(utter_actions, utter)
+            if not found:
+                logger.error('The utter ' + utter+ ' is not listed in actions')
+
+    def verify_utters_in_stories(self):
+        if self.valid_utters == []:
+            self.verify_utters() 
+        
+        for file in self.stories:
+            f = open(file, 'r')
+            stories_lines = f.readlines()
+            f.close()
+
+            for line in stories_lines:
+                s_line = line.split()
+                if len(s_line) == 2 and s_line[0] == '-':  
+                    utter = s_line[1]
+                    found = self.search(self.valid_utters, utter)
+                    if not found:
+                        logger.error('The utter '+ utter +' is used in the stories'+
+                                     ' story file '+ file + ' (line: '+
+                                     str(stories_lines.index(line)+1) +
+                                     ') but it\'s not a valid utter.')
+
+    def verify_utters_being_used(self):
+        if self.valid_utters == []:
+            self.verify_utters() 
+        
+        stories_utters = []
+        for file in self.stories:
+            f = open(file, 'r')
+            stories_lines = f.readlines()
+            f.close()
+
+            for line in stories_lines:
+                s_line = line.split()
+                if len(s_line) == 2 and s_line[0] == '-':
+                    utter = s_line[1]
+                    stories_utters.append(utter)
+
+        for utter in self.valid_utters:
+            found = self.search(stories_utters, utter)
+            if not found:
+                logger.warning('The utter ' + utter + ' is not being used in any story')
+
     def run_verifications(self):
         self.verify_domain()
         self.verify_intents()
         self.verify_intents_in_stories()
         self.verify_intents_being_used()
+        self.verify_utters()
+        self.verify_utters_in_stories()
+        self.verify_utters_being_used()
 
 if __name__ == '__main__':
     domain = parser.parse_args().domain
     stories = parser.parse_args().stories
     intents = parser.parse_args().intents
     warning = parser.parse_args().warnings
+    validate_intents = parser.parse_args().validate_intents
+    validate_utters = parser.parse_args().validate_utters
+    validate_domain = parser.parse_args().validate_domain
 
     validator = Validator(domain, intents, stories)
-    validator.verify_domain()
-    validator.verify_intents()
-    validator.verify_intents_in_stories()
     
-    print(warning)
+    if validate_domain:
+        validator.verify_domain()
+    
+    if validate_intents:
+        validator.verify_intents()
+        validator.verify_intents_in_stories()
+        if warning:
+            validator.verify_intents_being_used()
 
-    if warning:
-        validator.verify_intents_being_used()
-
-
+    if validate_utters:
+        validator.verify_utters()
+        validator.verify_utters_in_stories()
+        if warning:
+            validator.verify_utters_being_used()
     
