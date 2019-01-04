1c22ea4121d233b947acef61a783956f901aaede
arthurTemporim <arthurrtl@gmail.com>
2019-01-04 18:06:22 -0200

    Correct docker test container, add scripts for tests integration and correct tests.
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

--- docker/run_test.sh
+++ docker/run_test.sh
@@ -1,3 +1,9 @@
-sleep 5
+echo "Waiting bot container up."
+until ping -c1 bot &>/dev/null; do :; done
+echo "Bot container up."
+
+echo "Waiting bot up."
+until curl bot:5005 &>/dev/null; do :; done
+echo "Bot up."
 
 pytest -s test.py
