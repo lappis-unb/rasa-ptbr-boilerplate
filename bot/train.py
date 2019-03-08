dac4b65c2dbc634dc2f5d0d4206a5542ec60e6c9
Arthur <arthurrtl@gmail.com>
2019-03-08 18:18:43 -0300

    Update rasa core and rasa nlu version
    Signed-off-by: Arthur <arthurrtl@gmail.com>

--- bot/train.py
+++ bot/train.py
@@ -1,66 +1,26 @@
 import logging
-import os
 
-from rasa_core import utils
-from rasa_core.agent import Agent
-from rasa_core.featurizers import MaxHistoryTrackerFeaturizer, BinarySingleStateFeaturizer, LabelTokenizerSingleStateFeaturizer,FullDialogueTrackerFeaturizer
-from rasa_core.policies.keras_policy import KerasPolicy
-from rasa_core.policies.memoization import MemoizationPolicy, AugmentedMemoizationPolicy
-from rasa_core.policies.fallback import FallbackPolicy
-from fallback import CustomFallbackPolicy
-
+from rasa_core import utils, train
 from validator import Validator
 
 logger = logging.getLogger(__name__)
-NLU_THRESHOLD = float(os.getenv('NLU_THRESHOLD', 0.6))
-
-TRAINING_EPOCHS = int(os.getenv('TRAINING_EPOCHS', 10))
-BATCH_SIZE = int(os.getenv('BATCH_SIZE', 10))
-VALIDATION_SPLIT = float(os.getenv('VALIDATION_SPLIT', 0.2))
-
-
-CORE_THRESHOLD = float(os.getenv('CORE_THRESHOLD', 0.6))
-MAX_HISTORY = int(os.getenv('MAX_HISTORY', 2))
-FALLBACK_ACTION_NAME = str(os.getenv('FALLBACK_ACTION_NAME', 'utter_default'))
 
 utils.configure_colored_logging(loglevel='DEBUG')
 
-def train_dialogue(domain_file, model_path, training_folder):
-    MemoizationPolicy.USE_NLU_CONFIDENCE_AS_SCORE = True
-    #keras_1 = KerasPolicy(
-    #             MaxHistoryTrackerFeaturizer(
-    #                 BinarySingleStateFeaturizer(),
-    #                 max_history=MAX_HISTORY
-    #                 )
-    #             )
-    keras_2 = KerasPolicy(
-                FullDialogueTrackerFeaturizer(
-                    LabelTokenizerSingleStateFeaturizer()
-                )
-            )
-    #agent = Agent(domain_file, policies=[
-    #        keras_2,
-    #        MemoizationPolicy(max_history=MAX_HISTORY),
-    #                                                CustomFallbackPolicy(
-    #                    fallback_action_name=FALLBACK_ACTION_NAME,
-    #                    nlu_threshold=NLU_THRESHOLD,
-    #                    core_threshold=CORE_THRESHOLD)])
 
-    agent = Agent(domain_file, policies=[
-            keras_2,
-            MemoizationPolicy(max_history=MAX_HISTORY),
-                                                    FallbackPolicy(
-                        nlu_threshold=NLU_THRESHOLD,
-                        core_threshold=CORE_THRESHOLD)])
-
-    training_data = agent.load_data(training_folder,augmentation_factor=20)
-
-    agent.train(training_data, epochs=TRAINING_EPOCHS, 
-                                batch_size=BATCH_SIZE,
-                                validation_split=VALIDATION_SPLIT)
-    agent.persist(model_path)
+def train_dialogue(domain_file,
+                   model_path,
+                   training_folder,
+                   policy_config):
+    return train.train_dialogue_model(domain_file=domain_file,
+                                      stories_file=training_folder,
+                                      output_path=model_path,
+                                      policy_config=policy_config,
+                                      kwargs={'augmentation_factor': 20,
+                                              'validation_split': 0.2,}
+                                      )
 
 if __name__ == "__main__":
     validate = Validator('domain.yml','data/intents', 'data/stories/' )
     validate.run_verifications()
-    train_dialogue('domain.yml', 'models/dialogue', 'data/stories/')
+    train_dialogue('domain.yml', 'models/dialogue', 'data/stories/', 'policy_config.yml')
