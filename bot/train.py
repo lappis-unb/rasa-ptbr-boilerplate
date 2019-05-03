949d99848265f085a7de12442efe49ba5738b205
arthurTemporim <arthurrtl@gmail.com>
2019-05-03 18:07:03 -0300

    Update rasa-core==0.14.1 and rasa-nlu==0.15.0 and solve updates issues
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

--- bot/train.py
+++ bot/train.py
@@ -12,7 +12,7 @@ def train_dialogue(domain_file,
                    model_path,
                    training_folder,
                    policy_config):
-    return train.train_dialogue_model(domain_file=domain_file,
+    return train(domain_file=domain_file,
                                       stories_file=training_folder,
                                       output_path=model_path,
                                       policy_config=policy_config,
