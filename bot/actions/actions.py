d2faa0535bd117abfa542a2c311f6ad28b640879
guilacerda <guiglacerda@gmail.com>
2019-06-10 22:58:46 -0300

    Sends message to another bot
    Co-authored-by: pablodiegoss <pablodiegoss@hotmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,4 +1,9 @@
 from rasa_core_sdk import Action
+import requests
+import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ActionTest(Action):
@@ -19,5 +24,16 @@ class ActionFallback(Action):
     def run(self, dispatcher, tracker, domain):
         text = ''
         text = tracker.latest_message.get('text')
+        r = ''
+        payload = {'query': text}
+        payload = json.dumps(payload)
+        header = {"content-type": "application/json"}
 
-        dispatcher.utter_message(text)
+        r = requests.post("http://tais:5005/conversations/default/respond",
+                          data=payload, headers=header)
+        logger.warning(str(r.text))
+        r = r.text
+        # logger.warning("************************" + r[0] + "*********************************")
+        r = json.loads(r)
+        # logger.warning("teste " + r["text"])
+        dispatcher.utter_message("ola")
