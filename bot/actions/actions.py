e2448170a7a2035996e2eba71bb46ce28afcf34c
pablodiegoss <pablodiegoss@hotmail.com>
2019-06-12 17:16:32 -0300

    Change action desired feature
    Co-authored-by: guilacerda <guiglacerda@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -25,25 +25,67 @@ class ActionFallback(Action):
         text = ''
         text = tracker.latest_message.get('text')
 
-        self.send_message(dispatcher, tracker, text)
-        self.get_message_tracker(dispatcher, tracker, text)
+        bots = ["tais:5005"]
 
-    def send_message(self, dispatcher, tracker, text):
+        answers = self.ask_bots(text, bots)
+
+        answer = self.get_best_answer(answers)
+        
+        logger.debug("Remote Utter: " + answer.info.utter)
+        logger.debug("Remote Intent: " + answer.info.intent)
+        logger.debug("Remote Confidence: " + answer.confidence)
+        
+        for message in answer.messages:
+            dispatcher.utter_message(message)
+
+
+    def get_best_answer(self, answers):
+        max_confidence = max([answer['confidence'] for answer in answers])
+        best_answer = {}
+        for answer in answers:
+            if(answer.confidence >= max_confidence):
+                best_answer = answer
+        return best_answer
+
+
+    def ask_bots(self, text, bots):
+        answers = []
+        for bot in bots:
+            messages = self.send_message(text, bot)
+            info = self.get_message_info(text, bot)
+            bot_answer = {
+                "messages": messages,
+                "info": info,
+                "confidence": info.confidence
+            }
+            answers.append(bot_answer)
+
+        return answers
+
+
+    def send_message(self, text, bot_url):
         payload = {'query': text}
         payload = json.dumps(payload)
 
         r = post_request(payload, "http://" + bot_url + "/conversations/default/respond")
         messages = []
+        for i in range(0, len(r)):
+            messages.append(r[i]['text'])
+        return messages
 
-        for i in range(0, len(r.json())):
-            dispatcher.utter_message(r.json()[i]['text'])
-
-    def get_message_tracker(self, dispatcher, tracker, text):
+    def get_message_info(self, text, bot_url):
         payload = {'query': text}
         payload = json.dumps(payload)
 
         r = get_request(payload, "http://" + bot_url + "/conversations/default/tracker")
-        for event in r['events']:
-            if 'parse_data' in event:
-                logger.warning(event['text'])
-                logger.warning(event['parse_data']['intent']['confidence'])
+        
+        for events in data['events']:
+            if 'event' in events:
+                if 'action' in events['event']:
+                    event_tracker.append(['Utter: ', events['name']])
+            if 'parse_data' in events:
+                event_tracker.append(['User Message: ', events['text']])
+                event_tracker.append(['Intent: ', events['parse_data']['intent']['name']])
+                event_tracker.append(['Confidence: ', events['parse_data']['intent']['confidence']])
+event_tracker.append('-'*50)
+        return message_info
