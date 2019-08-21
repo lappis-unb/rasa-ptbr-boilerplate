b59bbe052451c3b42c0b29fa5a197359747775b4
Pablo Diego <pablodiegoss@hotmail.com>
2019-08-21 14:30:43 -0300

    Add main to model_downloader.py

--- scripts/model_downloader.py
+++ scripts/model_downloader.py
@@ -4,12 +4,12 @@ import wget
 import hashlib
 import time
 
+
 MODEL_FOLDER = "/models" 
 MODEL_FILENAME = "models.tar.gz"
 MODEL_HOST = os.getenv('COACH_URL', 'coach')
 VERSION_URL = "http://" + MODEL_HOST + "/version"
 FILE_URL = 'http://' + MODEL_HOST + '/' + MODEL_FILENAME
-
 
 
 def try_connect_coach():
@@ -39,28 +39,30 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest() + "  " + fname + "\n"
 
+
 def uncompress_models(filename):
     os.system("tar -xzvf " + filename)
     os.system("mv src_models " + MODEL_FOLDER)
 
 
+if __name__ == '__main__':
 
-try_connect_coach()
-
-if os.path.isfile(MODEL_FILENAME) == True:
-    print("file already exist")
-    r = requests.get(url = VERSION_URL)
-    file_hash = md5(MODEL_FILENAME)
-    if r.text != file_hash:
-        print("new version model file")
-        os.system("rm -r " + MODEL_FILENAME)
-        os.system("rm -rf " + MODEL_FOLDER)
+    try_connect_coach()
+    
+    if os.path.isfile(MODEL_FILENAME) == True:
+        print("file already exist")
+        r = requests.get(url = VERSION_URL)
+        file_hash = md5(MODEL_FILENAME)
+        if r.text != file_hash:
+            print("new version model file")
+            os.system("rm -r " + MODEL_FILENAME)
+            os.system("rm -rf " + MODEL_FOLDER)
+            wget.download(FILE_URL)
+            uncompress_models(MODEL_FILENAME)
+        else:
+            print("same model")
+            uncompress_models(MODEL_FILENAME)
+    else:
+        print("model not found")
         wget.download(FILE_URL)
         uncompress_models(MODEL_FILENAME)
-    else:
-        print("same model")
-        uncompress_models(MODEL_FILENAME)
-else:
-    print("model not found")
-    wget.download(FILE_URL)
-    uncompress_models(MODEL_FILENAME)
