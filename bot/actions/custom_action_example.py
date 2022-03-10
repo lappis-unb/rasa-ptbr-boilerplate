96df05231bc5cf82cd695c7d644db2ad3998f020
arthurtemporim <arthurrtl@gmail.com>
2022-03-10 00:28:10 -0300

    Apply black
    Signed-off-by: arthurtemporim <arthurrtl@gmail.com>

--- bot/actions/custom_action_example.py
+++ bot/actions/custom_action_example.py
@@ -14,15 +14,16 @@ from rasa_sdk.executor import CollectingDispatcher
 
 
 class ActionExemplo(Action):
-
     def name(self) -> Text:
         return "action_exemplo"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(text="Esta mensagem esta sendo enviada do RASA SDK!!!")
 
         return []
-
