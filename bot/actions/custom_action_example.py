7356192b2f4a5196673174acf2aa44f89cc0be7b
arthurtemporim <arthurrtl@gmail.com>
2023-01-28 00:04:02 -0300

    Add action_default_fallback
    Signed-off-by: arthurtemporim <arthurrtl@gmail.com>

--- bot/actions/custom_action_example.py
+++ bot/actions/custom_action_example.py
@@ -2,6 +2,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
 
 
 class ActionExemplo(Action):
@@ -16,3 +17,22 @@ class ActionExemplo(Action):
     ) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(text="Esta mensagem esta sendo enviada do RASA SDK!!!")
         return []
+
+
+class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_core_fallback")
+
+        # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
